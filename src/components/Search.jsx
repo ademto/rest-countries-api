5bc758aec69b598b8143587ef7df37186e90e9a1
Emmanuel Adetoro <112830359+ademto@users.noreply.github.com>
2025-06-08 23:56:14 -0400

    search updates

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react';
 import { FaSearch } from "react-icons/fa";
+import { useSearchParams } from "react-router-dom";
 
 export default function Search() {
-
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [search, setSearch] = useState(searchParams.get("search") || "");
+    setSearchParams({ search });
     return (
         <div className="shadow-sm p-4 bg-white dark:bg-dark-elements dark:text-white rounded-sm flex items-center gap-5 w-full md:w-96">
             <FaSearch />
-            <input className="w-full outline-0 dark:text-white" type="text" name="search" id="search" placeholder="Search for a country"/>
+            <input 
+                onChange={(e) => setSearchParams({ search: e.target.value })}
+                className="w-full outline-0 dark:text-white" type="text" 
+                name="search" id="search" placeholder="Search for a country"
+            />
         </div>
     )
 }
